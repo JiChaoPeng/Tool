23b0d4c13542229410a7eeadc57b3752d4cefc34
jichaopeng <jichaopeng@foxmail.com>
2020-02-27 12:04:07 +0800

    - ad: add master

---
+++ app/src/main/java/com/android/tool/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.android.tool
+
+import android.os.Bundle
+import com.android.frameworktool.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
