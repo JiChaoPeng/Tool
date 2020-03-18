a4082fd62526850887f6f1b2714c7d06be198bfe
jichaopeng <jichaopeng@foxmail.com>
2020-03-18 22:38:07 +0800

    - mf: 修改网络请求

--- frameworktool/src/main/java/com/android/frameworktool/util/NetWorkManager.kt
+++ frameworktool/src/main/java/com/android/frameworktool/util/NetWorkManager.kt
@@ -1,7 +1,11 @@
 package com.android.frameworktool.util
 
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -10,14 +14,23 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 class NetWorkManager {
 
-    fun initNetWorkManager(baseUrl: String) {
+    fun initNetWorkManager(baseUrl: String): Retrofit {
+        val okHttpClient: OkHttpClient by lazy {
+            OkHttpClient().newBuilder().apply {
+                connectTimeout(20, TimeUnit.SECONDS)
+                readTimeout(20, TimeUnit.SECONDS)
+                writeTimeout(20, TimeUnit.SECONDS)
+            }.build()
+        }
         /**
          * 创建Retrofit实例时需要通过Retrofit.Builder,并调用baseUrl方法设置URL。
          * Retrofit2的baseUlr 必须以 /（斜线）结束，不然会抛出一个IllegalArgumentException
          */
-        var retrofit = Retrofit.Builder()
+        return Retrofit.Builder()
             .baseUrl(baseUrl) // 设置 网络请求 Url
             .addConverterFactory(GsonConverterFactory.create()) //设置使用Gson解析(记得加入依赖)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .client(okHttpClient)
             .build()
     }
 
