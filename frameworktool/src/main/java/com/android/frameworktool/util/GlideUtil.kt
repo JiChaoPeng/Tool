2ae5e354da3bc762bc2f9237cec71fd86c4cf57e
jichaopeng <jichaopeng@foxmail.com>
2020-03-30 19:52:08 +0800

    - mf: add adress

--- frameworktool/src/main/java/com/android/frameworktool/util/GlideUtil.kt
+++ frameworktool/src/main/java/com/android/frameworktool/util/GlideUtil.kt
@@ -3,14 +3,22 @@ package com.android.frameworktool.util
 import android.net.Uri
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
 /**
  * Created by jichaopeng
  * 2020/3/18
  */
+fun getImageOptions():RequestOptions{
+   return RequestOptions()
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+
+}
 fun loadImage(imageView: ImageView, uri: Uri, width: Int) {
     Glide.with(imageView.context)
         .load(uri)
+        .apply(getImageOptions())
         .override(width, width)
         .into(imageView)
 
@@ -19,12 +27,14 @@ fun loadImage(imageView: ImageView, uri: Uri, width: Int) {
 fun loadImage(imageView: ImageView, uri: Uri) {
     Glide.with(imageView.context)
         .load(uri)
+        .apply(getImageOptions())
         .into(imageView)
 }
 
 fun loadImage(imageView: ImageView, path: String, width: Int) {
     Glide.with(imageView.context)
         .load(path)
+        .apply(getImageOptions())
         .override(width, width)
         .into(imageView)
 }
@@ -32,5 +42,6 @@ fun loadImage(imageView: ImageView, path: String, width: Int) {
 fun loadImage(imageView: ImageView, path: String) {
     Glide.with(imageView.context)
         .load(path)
+        .apply(getImageOptions())
         .into(imageView)
 }
